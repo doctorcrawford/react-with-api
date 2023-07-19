e20398e65859937f119ed0e15b8119e8449069d3
Kyle Crawford <kcraw217@gmail.com>
2023-07-19 11:15:38 -0700

    add get top stories success and failure

--- src/__tests__/reducers/top-stories-reducer.test.js
+++ src/__tests__/reducers/top-stories-reducer.test.js
@@ -1,6 +1,9 @@
+import * as c from '../../actions/ActionTypes';
 import topStoriesReducer from '../../reducers/top-stories-reducer';
 
 describe('topStoriesReducer', () => {
+
+  let action;
 
   const initialState = {
     isLoaded: false,
@@ -14,5 +17,33 @@ describe('topStoriesReducer', () => {
         topStoriesReducer(initialState, { type: null })
       }
     ).toThrowError('There is no action matching null.')
+  });
+
+  test('successfully getting top stories should change isLoaded to true and update topStories', () => {
+    const topStories = 'An article';
+    action = {
+      type: c.GET_TOP_STORIES_SUCCESS,
+      topStories
+    };
+
+    expect(topStoriesReducer(initialState, action)).toEqual({
+      isLoaded: true,
+      topStories: 'An article',
+      error: null
+    });
+  });
+
+  test('failint to get topStores should change isLoaded to true and add an error message', () => {
+    const error = 'An error';
+    action = {
+      type: c.GET_TOP_STORIES_FAILURE,
+      error
+    };
+
+    expect(topStoriesReducer(initialState, action)).toEqual({
+      isLoaded: true,
+      topStories: [],
+      error: 'An error'
+    });
   });
 });
